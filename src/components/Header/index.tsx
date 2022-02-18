59875f377081c9e7a1dd9479ce3fa058d6e9e529
isamrish <askmaurya48@gmail.com>
2022-02-18 11:17:39 +0530

    Add new page and fix navbar
    Signed-off-by: isamrish <askmaurya48@gmail.com>

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,37 +1,24 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { Link } from "gatsby";
-import * as React from "react"
-import { AlignRight, X  } from "react-feather";
-import { useDevice } from "../../hooks/useDevice";
+import * as React from "react";
+// import { AlignRight, X } from "react-feather";
+// import { useDevice } from "../../hooks/useDevice";
 import { NavLinks } from "./nav-links";
 import "./style.scss";
 export const Header = () => {
-    const { device } = useDevice();
-    const [ show, setShow ] = useState(false);
-    const handleMobile = () => {
-        setShow(!show);
-    }
-    return(
-        <>
-        <div className="header">
-            <Link to="/" className="logo">
-                <h2>Amrish Kushwaha</h2>
-            </Link>
-            {device === 'mobile' && !show && (
-                <AlignRight onClick={() => handleMobile()} />
-            ) }
-            {device === 'mobile' && show && (
-                <X onClick={() => handleMobile()} />
-            ) }
-            {(device === "tablet" || device === "desktop") && (
-                <NavLinks />
-            )}
-        </div>
-        {device === 'mobile' && show && (
-            <div className="mobile-nav">
-                <NavLinks className="mobile-nav__links" />
-            </div>
-        )}
-        </>
-    )
-}
+  //   const { device } = useDevice();
+  //   const [show, setShow] = useState(false);
+  //   const handleMobile = () => {
+  //     setShow(!show);
+  //   };
+  return (
+    <>
+      <div className="header">
+        <Link to="/" className="logo">
+          <h2>Amrish Kushwaha</h2>
+        </Link>
+        <NavLinks />
+      </div>
+    </>
+  );
+};
--- src/components/Header/nav-links.tsx
+++ src/components/Header/nav-links.tsx
@@ -1,17 +1,20 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import "./style.scss";
-export const NavLinks = (props) => {
-    return(
-        <div className="nav" {...props}>
-            <ul className="nav__list">
-                <li className="nav__item">
-                    <Link to="/blog">Blog</Link>
-                </li>
-                <li className="nav__item">
-                    <Link to="/about">About</Link>
-                </li>
-            </ul>
-        </div>
-    )
-}
+export const NavLinks = (props: any) => {
+  return (
+    <div className="nav" {...props}>
+      <ul className="nav__list">
+        <li className="nav__item">
+          <Link to="/blog">Code</Link>
+        </li>
+        <li className="nav__item">
+          <Link to="/productivity">Productivity</Link>
+        </li>
+        <li className="nav__item">
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
