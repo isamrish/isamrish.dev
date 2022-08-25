3332146a05a499d693496599f1d3cab6a47798d1
isamrish <askmaurya48@gmail.com>
2022-08-26 02:30:52 +0530

    add adsense
    Signed-off-by: isamrish <askmaurya48@gmail.com>

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -3,11 +3,18 @@ import { Helmet } from "react-helmet";
 
 const Head = ({ title }: { title: string }) => {
   return (
-    <Helmet
-      defaultTitle="Welcome | IsAmrish"
-      title={title}
-      titleTemplate="%s | IsAmrish"
-    />
+    <>
+      <Helmet
+        defaultTitle="Welcome | IsAmrish"
+        title={title}
+        titleTemplate="%s | IsAmrish"
+      />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9666200095847888"
+        crossOrigin="anonymous"
+      />
+    </>
   );
 };
 
